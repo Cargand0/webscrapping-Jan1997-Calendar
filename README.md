55658b03d182c04c4a225214e384d8cc488d9bcb
Ahmad Fudhail <ahmadfudhail27@gmail.com>
2023-04-22 22:39:33 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Webscrapping-Website-Calendar
+ In this project, I will scrape data from January 1997 calendar.
---
+++ jan1997-calendar.py
@@ -0,0 +1,39 @@
+#import library
+import csv
+from urllib .request import Request, urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+#web url
+myUrl = 'https://www.timeanddate.com/calendar/?year=1997&country=69'
+
+#request
+uClient = uReq(myUrl)
+page_html = uClient.read()
+uClient.close()
+
+#parse page
+page_soup = soup(page_html,"html.parser")
+
+#create csv
+filename = "january1997.csv"
+f = open(filename, "w", newline='')
+writer = csv.writer(f)
+headers = ["Day"]
+writer.writerow(headers)
+
+#find calendar table on page
+calendar_table = page_soup.find("table", {"class": "ca ca1"})
+
+"""if calendar_table is not None:
+    print("found")
+else:
+    print("table not found")"""
+
+#extract data from calendar
+calendar_data = []
+for row in calendar_table.find_all("tr"):
+    calendar_row = []
+    for cell in row.find_all("tr",{"class": "c1"}):
+        day = cell.find("td",{"class":"cr"})
+        if day:
+            writer.writerow([day])
